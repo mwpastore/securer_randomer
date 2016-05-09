926a4047f7fca8ebf4af6a74a7c436d2e9e4640c
Mike Pastore <mike@oobak.org>
2016-05-09 01:02:01 -0500

    Enhance compatibility with Ruby 2.3

--- lib/securer_randomer/monkeypatch/secure_random.rb
+++ lib/securer_randomer/monkeypatch/secure_random.rb
@@ -5,27 +5,48 @@ require 'rbnacl/util'
 require 'rbnacl/random'
 
 module SecureRandom
-  # for Rubies < 2.2
-  def self.random_bytes(n = nil)
-    RbNaCl::Random.random_bytes(n ? n.to_i : 16)
+  RUBY_VER_OBJ = Gem::Version.new(RUBY_VERSION.dup)
+  RUBY_GE_2_2 = RUBY_VER_OBJ >= Gem::Version.new(String.new('2.2.0'))
+  RUBY_GE_2_3 = RUBY_VER_OBJ >= Gem::Version.new(String.new('2.3.0'))
+
+  if RUBY_GE_2_2
+    def self.gen_random(n)
+      RbNaCl::Random.random_bytes(n)
+    end
+  else
+    def self.random_bytes(n = nil)
+      RbNaCl::Random.random_bytes(n ? n.to_i : 16)
+    end
   end
 
-  # for Rubies >= 2.2
-  def self.gen_random(n)
-    RbNaCl::Random.random_bytes(n)
-  end
+  if RUBY_GE_2_3
+    def self.random_number(n = 0)
+      case n
+      when nil
+        Kernel.rand
+      when Range
+        raise ArgumentError, "invalid argument - #{n}" \
+          unless n.begin.is_a?(Numeric) and n.end.is_a?(Numeric)
 
-  def self.random_number(n = 0)
-    # mimic exceptions raised by "stock" SecureRandom
-    raise ArgumentError, "comparison of Fixnum with #{n} failed" unless n.is_a?(Numeric)
-    if n.is_a?(Float) && n > 0
-      if Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new(String.new('2.2.0'))
-        raise TypeError, "Cannot convert into OpenSSL::BN"
+        Kernel.rand(n)
+      when Numeric
+        Kernel.rand(n > 0 ? n : 0)
       else
-        raise ArgumentError, 'wrong number of arguments'
+        raise ArgumentError, "invalid argument - #{n}"
       end
     end
+  else
+    def self.random_number(n = 0)
+      raise ArgumentError, "comparison of Fixnum with #{n} failed" unless n.is_a?(Numeric)
+      if n.is_a?(Float) && n > 0
+        if RUBY_GE_2_2
+          raise TypeError, "Cannot convert into OpenSSL::BN"
+        else
+          raise ArgumentError, 'wrong number of arguments'
+        end
+      end
 
-    Kernel.rand(n > 0 ? n : 0)
+      Kernel.rand(n > 0 ? n : 0)
+    end
   end
 end
--- spec/secure_random_spec.rb
+++ spec/secure_random_spec.rb
@@ -1,23 +1,6 @@
 require_relative 'spec_helper'
 
 describe SecureRandom do
-  context '.gen_random' do
-    context 'is monkeypatched' do
-      When(:source_location) { described_class.method(:gen_random).source_location }
-
-      Then { source_location }
-      And { source_location.first =~ %r{lib/securer_randomer/monkeypatch/secure_random\.rb$} }
-    end if ENV.fetch('WITH_MONKEYPATCH', 'true') == 'true'
-
-    context 'returns random bytes' do
-      When(:forty_bytes) { described_class.gen_random(40) }
-      When(:forty_other_bytes) { described_class.gen_random(40) }
-
-      Then { forty_bytes.bytesize == 40 }
-      And { forty_bytes != forty_other_bytes }
-    end
-  end if described_class.respond_to?(:gen_random)
-
   context '.random_number' do
     context 'is monkeypatched' do
       When(:source_location) { described_class.method(:random_number).source_location }
@@ -39,13 +22,13 @@ describe SecureRandom do
       When(:error_class) { Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new(String.new('2.2.0')) ? TypeError : ArgumentError }
 
       Then { result == Failure(error_class) }
-    end
+    end unless Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new(String.new('2.3.0'))
 
     context 'rejects ranges' do
       When(:result) { described_class.random_number(0..1) }
 
       Then { result == Failure(ArgumentError) }
-    end
+    end unless Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new(String.new('2.3.0'))
 
     context 'rejects strings' do
       When(:result) { described_class.random_number('a') }
@@ -57,7 +40,7 @@ describe SecureRandom do
       When(:result) { described_class.random_number(nil) }
 
       Then { result == Failure(ArgumentError) }
-    end
+    end unless Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new(String.new('2.3.0'))
 
     context 'has default behavior' do
       When(:floats) { Array.new(100) { described_class.random_number } }
@@ -76,7 +59,84 @@ describe SecureRandom do
       And { floats.all? { |f| f >= 0 } }
       And { floats.all? { |f| f < 1 } }
     end
+
+    context 'in ruby 2.3+' do
+      context 'ignores nil' do
+        Given(:samples) { [nil] }
+
+        When(:floats) { samples.map { |i| described_class.random_number(i) } }
+
+        Then { floats.all? { |f| f.is_a?(Float) } }
+        And { floats.all? { |f| f >= 0 } }
+        And { floats.all? { |f| f < 1 } }
+      end
+
+      context 'returns random integers in an inclusive range' do
+        When(:results) { Array.new(100) { described_class.random_number(4..10) } }
+
+        Then { results.all? { |i| i.is_a?(Integer) } }
+        And { results.all? { |i| i >= 4 } }
+        And { results.all? { |i| i <= 10 } }
+      end
+
+      context 'returns random integers in an exclusive range' do
+        When(:results) { Array.new(100) { described_class.random_number(4...10) } }
+
+        Then { results.all? { |i| i.is_a?(Integer) } }
+        And { results.all? { |i| i >= 4 } }
+        And { results.all? { |i| i < 10 } }
+      end
+
+      context 'returns random floats in an inclusive range' do
+        When(:results) { Array.new(100) { described_class.random_number(3.5..9.6) } }
+
+        Then { results.all? { |f| f.is_a?(Float) } }
+        And { results.all? { |f| f >= 3.5 } }
+        And { results.all? { |f| f <= 9.6 } }
+      end
+
+      context 'supports ranges including negative integers' do
+        When(:results) { Array.new(100) { described_class.random_number(-100..100) } }
+
+        Then { results.all? { |i| i.is_a?(Integer) } }
+        And { results.all? { |i| i >= -100 } }
+        And { results.all? { |i| i <= 100 } }
+        And { results.any? { |i| i < 0 } }
+      end
+
+      context 'supports ranges including negative floats' do
+        When(:results) { Array.new(100) { described_class.random_number(-100.0..100.0) } }
+
+        Then { results.all? { |f| f.is_a?(Float) } }
+        And { results.all? { |f| f >= -100.0 } }
+        And { results.all? { |f| f <= 100.0 } }
+        And { results.any? { |f| f < 0.0 } }
+      end
+
+      context 'rejects weird ranges' do
+        When(:result) { described_class.random_number('a'..'b') }
+
+        Then { result == Failure(ArgumentError) }
+      end
+    end if Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new(String.new('2.3.0'))
   end
+
+  context '.gen_random' do
+    context 'is monkeypatched' do
+      When(:source_location) { described_class.method(:gen_random).source_location }
+
+      Then { source_location }
+      And { source_location.first =~ %r{lib/securer_randomer/monkeypatch/secure_random\.rb$} }
+    end if ENV.fetch('WITH_MONKEYPATCH', 'true') == 'true'
+
+    context 'returns random bytes' do
+      When(:forty_bytes) { described_class.gen_random(40) }
+      When(:forty_other_bytes) { described_class.gen_random(40) }
+
+      Then { forty_bytes.bytesize == 40 }
+      And { forty_bytes != forty_other_bytes }
+    end
+  end if Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new(String.new('2.2.0'))
 
   context '.random_bytes' do
     context 'is monkeypatched' do
@@ -93,5 +153,5 @@ describe SecureRandom do
       Then { sixteen_bytes.bytesize == 16 }
       And { sixteen_bytes != sixteen_other_bytes }
     end
-  end
+  end unless Gem::Version.new(RUBY_VERSION.dup) >= Gem::Version.new(String.new('2.2.0'))
 end
