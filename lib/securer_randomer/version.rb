ba08f6149eba8255f49f6b78fbcb837c65c7ceee
Mike Pastore <mike@oobak.org>
2016-05-12 00:42:17 -0500

    Bump version to 0.1.7

--- lib/securer_randomer/version.rb
+++ lib/securer_randomer/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module SecurerRandomer
-  VERSION = '0.1.6' # rubocop:disable Style/MutableConstant
+  VERSION = '0.1.7' # rubocop:disable Style/MutableConstant
 end
